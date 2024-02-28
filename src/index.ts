92690957377ff2b379d878764a392519aa7d2ac7
“imac <imac@imac.com>
2024-02-28 10:05:32 +0200

    add express async handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { errorHandlerMiddleware, requestLoggerMiddleware } from "./middlewares";
 import { createPostHandler, listPostsHandler } from "./handlers/postHandler";
+import expressAsyncHandler from "express-async-handler";
 
 const app = express();
 const PORT = 3000;
@@ -8,9 +9,9 @@ const PORT = 3000;
 app.use(express.json());
 app.use(requestLoggerMiddleware);
 
-app.get("/posts", listPostsHandler);
+app.get("/posts", expressAsyncHandler(listPostsHandler));
 
-app.post("/posts", createPostHandler);
+app.post("/posts", expressAsyncHandler(createPostHandler));
 
 app.use(errorHandlerMiddleware);
 
