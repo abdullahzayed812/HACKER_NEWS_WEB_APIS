fcc16550cd96269e1da4fbe5b37579fd66bf5e64
abdullah <aabdullah.zayed0099@gmail.com>
2024-11-01 21:43:23 +0200

    add jwt parser middleware and enforce jwt middleware

--- server/middlewares/authMiddleware.ts
+++ server/middlewares/authMiddleware.ts
@@ -25,3 +25,37 @@ export const authMiddleware: ExpressHandler<any, any> = async (req, res, next) =
     res.status(401).send({ error: "Bad token" });
   }
 };
+
+export const jwtParseMiddleware: ExpressHandler<any, any> = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return next();
+  }
+
+  let payload: JwtObject;
+
+  try {
+    payload = verifyJwt(token);
+  } catch (e) {
+    const verifyErr = e as VerifyErrors;
+    if (verifyErr instanceof TokenExpiredError) {
+      return res.status(401).send({ error: ERRORS.TOKEN_EXPIRED });
+    }
+    return res.status(401).send({ error: ERRORS.BAD_TOKEN });
+  }
+
+  const user = await db.getUserById(payload.userId);
+  if (!user) {
+    return res.status(401).send({ error: ERRORS.USER_NOT_FOUND });
+  }
+  res.locals.userId = user.id;
+  return next();
+};
+
+export const enforceJwtMiddleware: ExpressHandler<any, any> = async (_, res, next) => {
+  if (!res.locals.userId) {
+    return res.sendStatus(401);
+  }
+  return next();
+};
